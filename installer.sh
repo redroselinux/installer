91df1beb2c9a5512f69f6b865f1032a76ca9a2a1
Juraj Kollár <mostypc7@gmail.com>
2025-11-26 19:52:45 +0100

    Rename file reference from ArchTitus to Installer

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #github-action genshdoc
 #
-# @file ArchTitus
+# @file Installer
 # @brief Entrance script that launches children scripts for each phase of installation.
 
 # Find the name of the folder the scripts are in
